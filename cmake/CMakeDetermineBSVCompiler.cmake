963ee9cdcf30847354f013519f939ad7694332fd
John W. Terrell <john@coolpeoplenetworks.com>
2021-10-25 22:50:30 -0700

    Refactor bsvc into bsvc and bsvar

--- cmake/CMakeDetermineBSVCompiler.cmake
+++ cmake/CMakeDetermineBSVCompiler.cmake
@@ -7,11 +7,12 @@ endif()
 _cmake_find_compiler(BSV)
 mark_as_advanced(CMAKE_BSV_COMPILER)
 
-set(CMAKE_BSV_AR ${CMAKE_CURRENT_LIST_DIR}/drivers/bsvc)
+set(CMAKE_AR ${CMAKE_CURRENT_LIST_DIR}/drivers/bsvar)
+set(CMAKE_BSV_LINKER ${CMAKE_CURRENT_LIST_DIR}/drivers/bsvlink)
+
 set(CMAKE_BSV_COMPILER_ID "BSVC")
 set(CMAKE_BSV_SOURCE_FILE_EXTENSIONS bsv;BSV)
 set(CMAKE_BSV_OUTPUT_EXTENSION .bo)
-
 set(CMAKE_BSV_COMPILER_ENV_VAR "BSV_COMPILER")
 
 # configure variables set in this file for fast reload later on
